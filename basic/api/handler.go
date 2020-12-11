72c56295de4f5792a5539025ff8cba7390377cd2
Ihor Semeniuk <semjuel07@gmail.com>
2020-12-11 13:56:47 +0200

    Add delay

--- basic/api/handler.go
+++ basic/api/handler.go
@@ -3,14 +3,21 @@ package api
 import (
 	"golang.org/x/net/context"
 	"log"
+	"math/rand"
 	"replicated_log/basic/model"
+	"time"
 )
 
 // Server represents the gRPC server.
 type Server struct{}
 
 func (s *Server) Send(ctx context.Context, in *LogMessage) (*LogMessage, error) {
-	log.Printf("gRPC received message: %s", in.Body)
+	log.Printf("gRPC received message: %s \n", in.Body)
+
+	rand.Seed(time.Now().UnixNano())
+	delay := rand.Intn(20)
+	log.Printf("Sleep for %d seconds \n", delay)
+	time.Sleep(time.Duration(delay) * time.Second)
 
 	model.AddMessage(model.Message{Text: in.Body})
 
