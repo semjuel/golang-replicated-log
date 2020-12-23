e74fa2e5cc8764361536e93781590f95e30025e6
Ihor Semeniuk <semjuel07@gmail.com>
2020-12-23 18:42:10 +0200

    Add random failure response

--- basic/api/handler.go
+++ basic/api/handler.go
@@ -8,6 +8,11 @@ import (
 	"time"
 )
 
+const (
+	SUCCESS = "success"
+	FAILURE = "failure"
+)
+
 // Server represents the gRPC server.
 type Server struct{}
 
@@ -19,7 +24,13 @@ func (s *Server) Send(ctx context.Context, in *LogMessage) (*LogMessage, error) 
 	log.Printf("Sleep for %d seconds \n", delay)
 	time.Sleep(time.Duration(delay) * time.Second)
 
-	model.AddMessage(model.Message{Text: in.Body})
+	model.AddMessage(model.Message{Id: in.Id, Text: in.Body})
 
-	return &LogMessage{Body: "ok"}, nil
+	status := SUCCESS
+	if delay%2 == 0 {
+		status = FAILURE
+	}
+	log.Printf("Response status - `%s`", status)
+
+	return &LogMessage{Body: status}, nil
 }
