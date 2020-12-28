b049a5f44366d1176ef7762e7e79987e9f3edb0b
Ihor Semeniuk <semjuel07@gmail.com>
2020-12-28 12:45:46 +0200

    Add spaces

--- master/utils/quorum.go
+++ master/utils/quorum.go
@@ -7,26 +7,32 @@ import (
 	"sync"
 	"sync/atomic"
 )
+
 func HasQuorum() error {
 	s := service.GetSecondaries()
 	var alive int32 = 1
+
 	wg := sync.WaitGroup{}
 	for _, v := range s {
 		wg.Add(1)
+
 		go func(v service.Secondary, alive *int32) {
 			err := IsAlive(v.HTTP)
 			log.Printf("Health error = %v", err)
+
 			if err == nil {
 				atomic.AddInt32(alive, 1)
 			}
 			wg.Done()
 		}(v, &alive)
 	}
+
 	wg.Wait()
 
 	log.Printf("Alive nodes %v", alive)
-	if (len(s) + 1) / 2 >= int(alive) {
+	if (len(s)+1)/2 >= int(alive) {
 		return errors.New("no quorum")
 	}
+
 	return nil
 }
